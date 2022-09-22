b8513dfd9ecf5b0ffc6cb98f5608dd7e37cc5e55
jgusta <jgustafx@gmail.com>
2022-09-22 04:50:21 -0700

    regen fresh

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,25 +4,18 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/rifff/[rifffId].tsx";
-import * as $5 from "./routes/rifff/_middleware.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/rifff/[rifffId].tsx";
+import * as $3 from "./routes/rifff/_middleware.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/about.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
-    "./routes/rifff/[rifffId].tsx": $4,
-    "./routes/rifff/_middleware.tsx": $5,
+    "./routes/index.tsx": $1,
+    "./routes/rifff/[rifffId].tsx": $2,
+    "./routes/rifff/_middleware.tsx": $3,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
