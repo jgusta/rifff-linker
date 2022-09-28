3291d98e250c41a44056a145208d72c6e153ba2e
jgusta <jgustafx@gmail.com>
2022-09-28 00:02:43 -0700

    Rifff page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,112 +1,11 @@
-import { apply } from "twind";
-import { asset, Head } from "$fresh/runtime.ts";
-import { PageProps } from "$fresh/server.ts";
-import { BASE_URL, ENVIRONMENT, GA_ID, metaDefaults } from "config";
-import { makeMeta } from "../util/metaDefaults.ts";
+import { Layout } from "@components/Layout.tsx";
 
-export const handler = {
-  GET: (_req, ctx) => {
-    return ctx.render({ ...ctx.state });
-  },
-};
 
-export const Nav = () => (
-  <div class="bg-teal-500">
-    <nav class="flex items-center justify-between flex-wrap max-w-screen-md mx-auto">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <a href="/">
-          <img
-            src={asset("/videopoker-academy-deno.svg")}
-            width={100}
-            alt="the video poker academy logo: deno the dino behind some cards"
-          />
-        </a>
-        <a href="/">
-          <span class="font-semibold text-xl tracking-tight">
-            Video Poker Academy
-          </span>
-        </a>
-      </div>
-    </nav>
-  </div>
-);
-
-export const Layout = (props:any) => {
-  const meta:typeof metaDefaults = makeMeta (props.meta);
-
-  console.log(meta);
+export default function Home() {
+  const metaProps = {}
   return (
     <>
-      <Head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        >
-        </meta>
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <meta name="description" content={meta.description} />
-
-        <meta property="og:site_name" content={meta.site_name}></meta>
-        <meta property="og:type" content="article"></meta>
-
-        <meta property="og:description" content={meta.description}></meta>
-        <meta property="og:image" content={meta.display_image}></meta>
-        <meta property="og:url" content={meta.share_url}></meta>
-        <meta property="og:title" content={meta.title}></meta>
-
-        <meta name="twitter:image:alt" content={meta.img_alt}></meta>
-        <meta name="twitter:card" content="summary_large_image"></meta>
-        {ENVIRONMENT === "production"
-          ? (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              >
-              </script>
-              <script
-                async
-                dangerouslySetInnerHTML={{
-                  __html:
-                    `window.dataLayer = window.dataLayer || [];function gtag() { 
-                    dataLayer.push(arguments); 
-                  }
-                  gtag('js', new Date()); 
-                  gtag('config', '${GA_ID}');`,
-                }}
-              >
-              </script>
-            </>
-          )
-          : <></>}
-
-        <title>{meta.title}</title>
-      </Head>
-      <Nav  />
-      {props.children}
-    </>
-  );
-};
-
-
-
-
-export default function Home(props:PageProps) {
-  const meta = {
-    description: "Create an easy-to-share link for your Endlesss rifffs",
-    site_name: "Endlesss Riff Linker",
-    display_image: `${BASE_URL}/splat.png`,
-    share_url: `${BASE_URL}`,
-    title: "Endlesss Rifff Viewer",
-    img_alt: "Endlesss Rifff Viewer",
-    user: "",
-  };
-
-  return (
-    <>
-      <Layout meta={meta}>
+      <Layout meta={metaProps}>
         <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
           <div class="sm:flex sm:items-center px-6 py-4">
             <img
--- routes/rifff/[rifffId].tsx
+++ routes/rifff/[rifffId].tsx
@@ -1,16 +1,10 @@
 import { PageProps } from "$fresh/server.ts";
 import { Handlers } from "$fresh/server.ts";
-import { BASE_URL } from "config";
-import { Layout } from "../index.tsx";
+import { BASE_URL, FROM, REFERER } from "config";
+import { Layout } from "@components/Layout.tsx";
+import { getRifffData, Rifff } from "@api/rifffData.ts";
 
-export async function getRiffData(rifffId: string) {
-  const resp = await fetch(
-    "https://endlesss.fm/api/v3/feed/shared_rifff/" + rifffId,
-  );
-  const data = await resp.json();
-
-  return data.data[0];
-}
+//endlesss.fm/jams/10709ed65be48dcc8381cf6022b25f3c34676689e013e9f7b6f55ecd9855d241?rifffId=cd5a89f03ebc11ed89f007f1ba9c2466
 
 export const handler: Handlers = {
   async GET(req, ctx) {
@@ -19,20 +13,21 @@ export const handler: Handlers = {
     if (rifffId == undefined) {
       throw new Error();
     }
-    const rifffData = await getRiffData(rifffId);
-    console.log(rifffData);
-    const resp = await ctx.render({ rifffData, rifffId });
-    return resp;
+    const rifff: Rifff = await getRifffData(rifffId);
+    console.log(rifff);
+    await Deno.writeTextFile("./hello.txt", JSON.stringify(rifff));
+    return await ctx.render(rifff);
   },
 };
 
-export default function RifffPage ({data}: PageProps) {
-  const { rifffData, rifffId } = data;
-  const shareImage = rifffData.image_url;
-  const time = rifffData.action_timestamp_iso;
-  const user = rifffData.user;
-  const feats = rifffData.creators.filter((x: any) => x != user).join(", ");
-  const title = `${user} - ${rifffData.title}`;
+export default function RifffPage(data: { data: Rifff } & PageProps) {
+  const rifff = data.data;
+  const shareImage = rifff.image ? rifff.image_url : "";
+  const time = rifff.action_timestamp_iso;
+  const user = rifff.user;
+  const rifffId = rifff._id;
+  const feats = rifff.creators.filter((x: string) => x != user).join(", ");
+  const title = `${user} - ${rifff.title}`;
   const shareUrl = `${BASE_URL}/rifff/${rifffId}`;
   const forwardUrl =
     `https://endlesss.fm/rifff-link?type=shared&rifffId=${rifffId}`;
