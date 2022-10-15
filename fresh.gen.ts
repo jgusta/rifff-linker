528fc2096c56a307c4120582cf994785d623b745
jgusta <jgustafx@gmail.com>
2022-10-15 01:52:01 -0700

    update config

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,22 +4,28 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_middleware.tsx";
-import * as $1 from "./routes/api/rifffData.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/rifff/[rifffId].tsx";
+import * as $1 from "./routes/api/fakeRifff.tsx";
+import * as $2 from "./routes/api/rifffData.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/rifff/[rifffId].tsx";
 import * as $$0 from "./islands/CopyButton.tsx";
 import * as $$1 from "./islands/LinkConvertor.tsx";
+import * as $$2 from "./islands/LinkHolder.tsx";
+import * as $$3 from "./islands/RifffCard.tsx";
 
 const manifest = {
   routes: {
     "./routes/_middleware.tsx": $0,
-    "./routes/api/rifffData.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/rifff/[rifffId].tsx": $3,
+    "./routes/api/fakeRifff.tsx": $1,
+    "./routes/api/rifffData.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/rifff/[rifffId].tsx": $4,
   },
   islands: {
     "./islands/CopyButton.tsx": $$0,
     "./islands/LinkConvertor.tsx": $$1,
+    "./islands/LinkHolder.tsx": $$2,
+    "./islands/RifffCard.tsx": $$3,
   },
   baseUrl: import.meta.url,
   config,
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,16 +1,28 @@
 import { Layout } from "@components/Layout.tsx";
 import LinkConvertor from "@islands/LinkConvertor.tsx";
-import { BASE_URL } from "config";
+import { BASE_URL, ENVIRONMENT, TEST_INPUT_LINK } from "config";
 
 export default function Home() {
   const metaProps = {};
-  return (
-    <>
-      <Layout meta={metaProps}>
-        <div class="bg-gray-600 my-10 px-8 py-4 mx-auto max-w-lg shadow-lg overflow-hidden">
-          <LinkConvertor baseUrl={BASE_URL}></LinkConvertor>
-        </div>
-      </Layout>
-    </>
-  );
+  if (TEST_INPUT_LINK && ENVIRONMENT === "development") {
+    return (
+      <>
+        <Layout meta={metaProps}>
+          <div class="bg-gray-600 my-10 px-8 py-4 mx-auto max-w-lg shadow-lg overflow-hidden">
+            <LinkConvertor baseUrl={BASE_URL} startValue={TEST_INPUT_LINK}/>
+          </div>
+        </Layout>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Layout meta={metaProps}>
+          <div class="bg-gray-600 my-10 px-8 py-4 mx-auto max-w-lg shadow-lg overflow-hidden">
+            <LinkConvertor baseUrl={BASE_URL} startValue="" />
+          </div>
+        </Layout>
+      </>
+    );
+  }
 }
