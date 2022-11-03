8e04da5b8112399f3d9fb1e6a75ccac48dae4470
jgusta <jgustafx@gmail.com>
2022-11-03 06:21:14 -0700

    tidy up and simplify

--- util/config.ts
+++ util/config.ts
@@ -5,7 +5,7 @@ const raw_env = Object.assign(Deno ? Deno.env.toObject() : {}, await config());
 
 // these are checked first so we can make a default BASE_URL
 const base_env = cleanEnv(raw_env, {
-  BASE_HOST: host({ default: "127.0.0.1" }),
+  BASE_HOST: host({ desc: "Mandatory hostname to serve as." }),
   PORT: port({ default: 8000 })
 });
 
@@ -22,6 +22,7 @@ else {
 
 const env = cleanEnv(raw_env, {
   API_BASE: str({ desc: 'ends with shared_rifff, no trailing' }),
+  BASE_HOST: str({ default: base_env.BASE_HOST }),
   BASE_URL: str({ default: base_url, desc: "The base url of the web site, no trailing slash. Autofilled using BASE_HOST and PORT, but can be overridden explicitly.", example: "https://www.example.com" }),
   ENVIRONMENT: str({ choices: ["development", "test", "production"], desc: "'development', 'test' or 'production'" }),
   FROM: email({ desc: "Contact email to identify service to Endlesss API", example: "youremail@example.com" }),
@@ -39,6 +40,7 @@ const env = cleanEnv(raw_env, {
 
 export const {
   API_BASE,
+  BASE_HOST,
   BASE_URL,
   ENVIRONMENT,
   FROM,
