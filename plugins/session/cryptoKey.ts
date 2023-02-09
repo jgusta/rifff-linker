111cae6957570de12eeb1c62e63b56db588343d4
jgusta <jgustafx@gmail.com>
2023-02-09 13:27:20 -0800

    fix async cryptoKey getting

--- plugins/session/cryptoKey.ts
+++ plugins/session/cryptoKey.ts
@@ -1,12 +1,16 @@
 import { stringToKey } from "./util.ts";
-let cryptoKey:CryptoKey|undefined;
+let cryptoKey: CryptoKey | undefined;
 
-export async function getKey(secret:string|undefined): Promise<CryptoKey> {
-  if (typeof cryptoKey == 'undefined') {
-    if (typeof secret == 'undefined') {
-      secret = (Deno.env.get('SESSION_SECRET') as string)
-    }
-    cryptoKey = await stringToKey(secret);
+export async function getKey(secret: string | undefined): Promise<CryptoKey> {
+  if (typeof cryptoKey != 'undefined') {
+    return Promise.resolve(cryptoKey)
   }
-  return cryptoKey;
+  if (typeof secret == 'undefined') {
+    secret = (Deno.env.get('SESSION_SECRET') as string)
+  }
+
+  const key_1 = await stringToKey(secret);
+  cryptoKey = key_1;
+  return key_1;
 }
+
