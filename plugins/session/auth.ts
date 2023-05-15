798f5f5e99949fb9d489891443d8cebf29b907e1
jgusta <jgustafx@gmail.com>
2023-05-14 18:49:50 -0700

    partially remove cookies

--- plugins/session/auth.ts
+++ plugins/session/auth.ts
@@ -17,44 +17,31 @@ class Auth<T> implements AuthBucket {
   user_id: string;
   expires: number;
   constructor(bucket: AuthBucket) {
-    this.token = bucket.token;
+    this.token    = bucket.token;
     this.password = bucket.password;
-    this.user_id = bucket.user_id;
-    this.expires = bucket.expires;
+    this.user_id  = bucket.user_id;
+    this.expires  = bucket.expires;
   }
 }
 
 export function getAuthFromCookies(headers: Headers) {
-  const cookies = readAuthCookies(headers)
-
+  const cookies = readAuthCookies(headers);
 }
 
 function setAuthCookies(data: AuthBucket, headers: Headers) {
-  const ex = new Date(data.expires);
+  
+  for (const key of authKeys) {
+    if (key === 'expires') {
+      const ex = new Date(data.expires);
+      setCookie(headers, { name: "e", path: "/", value: String(data.expires), expires: ex });
+    }
+    else {
+      setCookie(headers, { name: key, path: "/", value: data[key], expires: ex });
+    }
+  }
   setCookie(headers, { name: "t", path: "/", value: data.token, expires: ex });
   setCookie(headers, { name: "p", path: "/", value: data.password, expires: ex });
   setCookie(headers, { name: "i", path: "/", value: data.user_id, expires: ex });
-  setCookie(headers, { name: "e", path: "/", value: String(data.expires), expires: ex });
-}
-
-// Retrieve what is possibly a valid auth from cookies
-async function readAuthCookies(headers: Headers) {
-  const encryptedCookies = getCookies(headers)
-  const cypher = encryptedCookies.c;
-
-  try {
-    const key = await getKey();
-    const json = await decrypt(cypher, key);
-    const jBlob = JSON.parse(json);
-    const out = new Auth()
-    return out;
-  }
-  catch (e) {
-    console.log(e)
-    return {
-      expires: 0
-    }
-  }
 }
 
 export function logout(headers: Headers) {
