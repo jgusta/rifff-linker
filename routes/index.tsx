479261e996bc9fa88feec8ef9bac75c780885072
jgusta <jgustafx@gmail.com>
2023-05-14 18:50:45 -0700

    remove twind from index

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,18 +3,28 @@ import {
   ENVIRONMENT,
   TEST_INPUT_LINK,
 } from 'config';
-import { tw } from 'twind';
-
+import { css } from 'fresh_emotion';
 import Layout from '@components/Layout.tsx';
 import LinkConvertor from '@islands/LinkConvertor.tsx';
-
+const outerDiv = css`
+  background-color: #4b5563;
+  border-radius: 2rem;
+  box-shadow: 0px 25px 50px -12px rgba(0, 0, 0, 0.25);
+  margin-top: 2.5rem;
+  margin-bottom: 2.5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  overflow: hidden;
+`
 export default function Home() {
   const metaProps = {};
   if (TEST_INPUT_LINK && ENVIRONMENT === "development") {
     return (
       <>
         <Layout meta={metaProps}>
-          <div class={tw`bg-gray-600 rounded-2xl shadow-2xl my-10 px-8 py-4 mx-auto max-w-lg shadow-lg overflow-hidden`}>
+          <div class={outerDiv}>
             <LinkConvertor baseUrl={BASE_URL} startValue={TEST_INPUT_LINK}/>
           </div>
         </Layout>
@@ -24,7 +34,7 @@ export default function Home() {
     return (
       <>
         <Layout meta={metaProps}>
-          <div class={tw`bg-gray-600 rounded my-10 px-8 py-4 mx-auto max-w-lg shadow-lg overflow-hidden`}>
+          <div class={outerDiv}>
             <LinkConvertor baseUrl={BASE_URL} startValue="" />
           </div>
         </Layout>
