0813f702ab02865022f42ff1637d5d461b70feaa
jgusta <jgustafx@gmail.com>
2023-05-14 18:48:18 -0700

    use deps.ts pattern

---
+++ plugins/session/deps.ts
@@ -0,0 +1,6 @@
+export type { Plugin } from "$fresh/server.ts";
+export type { MiddlewareHandlerContext } from '$fresh/server.ts';
+export { getCookies } from "https://deno.land/std/http/cookie.ts";
+export { assert } from "https://deno.land/std/testing/asserts.ts";
+export { crypto } from "https://deno.land/std/crypto/crypto.ts";
+export { memoizy } from "https://deno.land/x/memoizy@1.0.0/mod.ts";
