24e70b720f0657ca91f39e0f44d66f8a1793af7b
jgusta <jgustafx@gmail.com>
2023-05-14 18:49:14 -0700

    rewire middleware

--- plugins/session/middleware.ts
+++ plugins/session/middleware.ts
@@ -1,11 +1,13 @@
-import { MiddlewareHandlerContext } from '$fresh/server.ts';
+import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import { Session } from "./session.ts";
+import { SessionExtension } from "./types.ts";
 
-import { startSession } from './mod.ts';
-
-export async function sessionMiddlewareHandler(req: Request, ctx: MiddlewareHandlerContext) {
-  const session = startSession(req);
-  ctx.state.session = session;
-  const res = await ctx.next();
-
-  return res;
+export function createSessionMiddlewareHandler(ext: SessionExtension) {
+  const sessionMiddlewareHandler = async function (req: Request, ctx: MiddlewareHandlerContext) {
+      const session = await Session.create(req, ext);
+      ctx.state.session = session;
+      const res = await ctx.next();
+      return res;
+  };
+  return sessionMiddlewareHandler;
 }
