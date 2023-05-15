158e39dbcb8c57f039a5823ce71a797776b39751
jgusta <jgustafx@gmail.com>
2023-05-14 18:50:17 -0700

    remove test

--- plugins/session/bin.ts
+++ plugins/session/bin.ts
@@ -1,22 +1,9 @@
-import { encrypt, decrypt, generateSecretKey, getKey } from "./crypto.ts";
+import { generateSecretKey } from "./crypto.ts";
 
-if (Deno.args[0] === 'genkey' || Deno.args[0] === 'gen-key') {
+if (Deno.args[0] === "genkey" || Deno.args[0] === "gen-key") {
   const key = await generateSecretKey();
-  console.log('Your key is:', key)
-  console.log('Add this to your .env:', `SESSION_SECRET=${key}`)
-} else if (Deno.args[0] === 'test') {
-
-  const myText = 'Hello the world Hello the worldHello the worldHello the worldHello the worldHello the world Hello the worldHello the worldHello the world  Hello the worldHello the worldHello the world';
-  console.log(`Plaintext to start: ${myText}`);
-  const key = await generateSecretKey();
-  console.log(`Secret Key: ${key}`);
-  const cryptoKey = await getKey(key);
-  const cyphered = await encrypt(myText, cryptoKey)
-  console.log(`Encrypted text: ${cyphered}`);
-  
-  const decyphered = await decrypt(cyphered, cryptoKey)
-  console.log(`Plaintext decrypted: ${decyphered}`);
-}
-else {
-  console.log('no command given.s')
+  console.log("Your key is:", key);
+  console.log("Add this to your .env:", `SESSION_SECRET=${key}`);
+} else {
+  console.log("no command given.");
 }
