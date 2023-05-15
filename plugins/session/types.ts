6c4209b20d8a7a50fbac205dfb529fb636510a2b
jgusta <jgustafx@gmail.com>
2023-05-14 18:48:52 -0700

    remove unnecessary types

--- plugins/session/types.ts
+++ plugins/session/types.ts
@@ -1,34 +1,24 @@
-import type { VNode } from 'preact';
-
-export interface LoginResponse extends Response {
-  json(): Promise<AuthBucket>
+import type { Session } from "./session.ts";
+export interface Cookies extends Record<string, string | number> {
+  expires: number;
+  [key: string]: string | number;
 }
 
-export interface AnyAuthBucket {
-  token?: string
-  password?: string
-  user_id?: string
-  expires: number
-}
+export type ExtStatus = string;
+export type SessionStatus = CookieStatus | ExtStatus;
 
-type Concrete<Type> = {
-  [Property in keyof Type]-?: Type[Property];
+export type ExtensionInput = {
+  data: Cookies;
+  status: CookieStatus;
+  headers: Headers;
+  session: Session;
 };
 
+export interface SessionExtension {
+  beforeRender(
+    extensionInput: ExtensionInput,
+  ): Promise<ExtStatus>;
+  inject(): Promise<string>;
+}
 
-export type AuthBucket = Concrete<AnyAuthBucket>;
-
-export type AuthKeys = Array<keyof AuthBucket>;
-
-// export interface AuthBucket extends AnyAuthBucket{
-//   token: string
-//   password: string
-//   user_id: string
-//   expires: number
-// }
-
-export type Maybe<T> = T | null;
-
-export type AuthChild = VNode & { auth: AuthBucket }
-
-export type AuthChildren = AuthChild[] | AuthChild | VNode[] | VNode;
+export type CookieStatus = "init" | "expired" | "valid";
