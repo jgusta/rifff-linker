dc20ea6b1c7d4b89b422387a50112def3f64e52e
jake <jgustafx@gmail.com>
2023-08-10 18:44:04 -0700

    cwntered layout to css

--- components/layout/CenteredLayout.tsx
+++ components/layout/CenteredLayout.tsx
@@ -1,10 +1,37 @@
 import { JSX } from 'preact/jsx-runtime';
+import { css, md, lg } from 'styles';
 
-export default function CenteredLayout (props: { children: JSX.Element[] }): JSX.Element {
+const style = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  margin-left: auto;
+  margin-right: auto;
+  height: 100vh;
+  ${md} {
+    height: auto;
+    padding:  3rem 0 0 0 ;
+    justify-content: center;
+    padding-top: 3rem;
+  }
+  ${lg} {
+    padding-top:0;
+    padding-bottom:0;
+  }
+  `
+
+const secBackCss = css`background-color: #1c1c1c;`;
+
+export default function CenteredLayout(props: { children: JSX.Element[] }): JSX.Element {
   return (
-  <section class="bg-gray-900">
-    <div class=" flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      {props.children}
-    </div>
-  </section>);
+    <section class={secBackCss}>
+      <div class={style}>
+        {props.children}
+      </div>
+    </section>
+  );
 }
