4a802fdc064d505bca137849aec20cc0800db15a
jake <jgustafx@gmail.com>
2023-08-10 18:42:00 -0700

    heading image

--- components/layout/HeadingImage.tsx
+++ components/layout/HeadingImage.tsx
@@ -1,14 +1,31 @@
-import { tw } from 'twind';
+import { css } from 'styles';
+
+const linkCss = css`  
+  margin-top: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  font-size: 2rem;
+  font-weight: 600;
+  color: #1a202c;
+`;
+
+const imgCss = css`
+  width: 8rem;
+  margin-right: 0.5rem;
+`;
 
 type Props = {
   src: string
   alt?: string
   ahref?: string
 }
-export default (props: Props) => {
-  const {ahref, src, alt} = props
+
+export default function HeadingImage(props: Props) {
+  const { ahref, src, alt } = props
   return (
-  <a href={ahref} class={tw`mt-6 flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white`}>
-    <img class="w-32 mr-2" src={src} alt={alt} />
-  </a>
-)}
+    <a href={ahref} class={linkCss}>
+      <img class={imgCss} src={src} alt={alt} />
+    </a>
+  )
+}
