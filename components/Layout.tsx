bb1f92f630b3d6343c8e7a40fca484aaac447626
jake <jgustafx@gmail.com>
2023-08-10 18:41:10 -0700

    layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,25 +1,29 @@
 import { JSX } from 'preact/jsx-runtime';
-import {css,cx} from 'fresh_emotion';
+import { css, cx } from 'fresh_emotion';
 import { PageMeta } from 'types';
 
-import Footer from '@components/Footer.tsx';
-import Nav from '@components/Nav.tsx';
-import PageHeading from '@components/PageHeading.tsx';
+import Footer from '@components/layout/Footer.tsx';
+import Nav from '@components/layout/Nav.tsx';
+import PageHeading from '@components/layout/PageHeading.tsx';
+
+const wrapper = css({
+  minHeight: "100vh",
+  display: "grid",
+  gap: "0px",
+  gridTemplateRows: "50px 1fr minmax(60px, auto)",
+  gridTemplateAreas: '"nav" "content" "footer"'
+});
+
+const bodyClass = css({
+  fontFamily: "sans-serif",
+})
 
 const Layout = ({ meta, children }: {
   meta: Partial<PageMeta>;
   children: JSX.Element;
 }) => {
-  const wrapper = css({
-    minHeight: "100vh",
-    display: "grid",
-    gap: "0px",
-    gridTemplateRows: "50px 1fr minmax(60px, auto)",
-    gridTemplateAreas: '"nav" "content" "footer"',
-  });
-    
   return (
-    <body class={tw`font-sans`}>
+    <body class={bodyClass}>
       <PageHeading meta={meta} />
       <div class={cx(wrapper, 'wrapper')}>
         <Nav tClass="main-nav" />
