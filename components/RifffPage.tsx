cb1b34ea1966146f208a9d45551d7f16694eadc1
jake <jgustafx@gmail.com>
2023-08-10 18:45:06 -0700

    Page layout

--- components/RifffPage.tsx
+++ components/RifffPage.tsx
@@ -1,10 +1,54 @@
-import { tw } from "twind";
+import {css} from 'fresh_emotion';
+import {md} from 'styles';
 import { PageProps } from "$fresh/server.ts";
 import { RifffWad } from "types";
 import Layout from "@components/Layout.tsx";
 import RifffCard from "@islands/RifffCard.tsx";
 import StatBlock from "@components/StatBlock.tsx";
 
+const btnHoldCss = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  height: 12rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  border: 1px solid white;
+  background-color: #4a5568;
+  &:hover {
+    background-color: #1a202c;
+  }
+`;
+
+const innerContCss = css`
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  position: relative;
+  ${md} {
+    max-width: 28rem;
+  }
+`
+const headTitleCss=`
+  max-width: 32rem;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  font-size: 3rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  color: white;
+  text-align: center;
+`;
+
+const btnLinkCss = css`text-align: center;color: #f6e05e;`;
+
+const fullWidthCss = css`width: 100%;`
 
 export default function RifffPage(props: RifffWad) {
   const { meta, rifffData, rifff } = props;
@@ -22,11 +66,11 @@ export default function RifffPage(props: RifffWad) {
 
   return (
     <Layout meta={meta}>
-      <div class={tw`w-full`}>
-        <h1 class={tw`max-w-lg m-auto flex flex-col px-8 text-3xl py-2 text-white text-center`}>{rifffData.title}</h1>
-        <div class={tw`max-w-md m-auto flex flex-col px-8 relative`}>
+      <div class={fullWidthCss}>
+        <h1 class={headTitleCss}>{rifffData.title}</h1>
+        <div class={innerContCss}>
           <RifffCard rifff={rifff} background={rifffData.display_image} />
-          <div class={tw`text-center border-1 border-white h-12 my-2 py-2 hover:bg-gray-900 bg-gray-700 flex flex-col justify-center`}><a class={tw`text-yellow-400 text-center`} href={`endlesss://sharedrifff/${rifffData.rifff_id}`}>Open in Endlesss</a></div>
+          <div class={btnHoldCss}><a class={btnLinkCss} href={`endlesss://sharedrifff/${rifffData.rifff_id}`}>Open in Endlesss</a></div>
           <StatBlock stats={stats} />
         </div>
       </div>
