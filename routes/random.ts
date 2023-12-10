e10e40f38a4490942f5765e9dc154419d1205bd8
jgusta <jgustafx@gmail.com>
2023-12-09 20:22:11 -0800

    fix

--- routes/random.ts
+++ routes/random.ts
@@ -4,13 +4,14 @@ import { env } from 'config';
 export const handler: Handlers = {
   async GET(req, ctx) {
     // const files = JSON.parse(await Deno.readTextFile("static/topRifffs.json"));
-    const files = JSON.parse(await import("../static/topRifffs.json", {
+
+    const top = await import("@/static/topRifffs.json", {
       with: { type: "json" },
-    }));
-    if (!Array.isArray(files)) {
+    })
+    if (!Array.isArray(top)) {
       throw new Error("Rifff list not found");
     }
-    const rifff_id = files[files.length * Math.random() | 0];
+    const rifff_id = top[(top.length * Math.random()) | 0]
     return new Response(new Blob(), {
       status: 307,
       headers: {
