8cac5ecadc5ae3380023546515249ed878ee58cf
jgusta <jgustafx@gmail.com>
2023-12-09 20:16:51 -0800

    fix json loading of random rifffs

--- routes/random.ts
+++ routes/random.ts
@@ -1,8 +1,12 @@
 import type { Handlers } from "$fresh/server.ts";
+// import file from "../static/topRifffs.json" with { type: "json" };
 import { env } from 'config';
 export const handler: Handlers = {
   async GET(req, ctx) {
-    const files = JSON.parse(await Deno.readTextFile("static/topRifffs.json"));
+    // const files = JSON.parse(await Deno.readTextFile("static/topRifffs.json"));
+    const files = JSON.parse(await import("../static/topRifffs.json", {
+      with: { type: "json" },
+    }));
     if (!Array.isArray(files)) {
       throw new Error("Rifff list not found");
     }
