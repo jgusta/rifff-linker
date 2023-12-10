c8ec5a9ac5e303c2b0f96af473eaec0d01473902
jgusta <jgustafx@gmail.com>
2023-12-09 20:41:06 -0800

    try to frix static

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,9 +4,9 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_middleware.tsx";
-import * as $1 from "./routes/api/auth.ts";
+import * as $1 from "./routes/files.ts";
 import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/login/index.tsx";
+import * as $3 from "./routes/random.ts";
 import * as $4 from "./routes/rifff/[rifffId].tsx";
 import * as $$0 from "./islands/CopyButton.tsx";
 import * as $$1 from "./islands/LinkConvertor.tsx";
@@ -18,9 +18,9 @@ import * as $$5 from "./islands/RifffCard.tsx";
 const manifest = {
   routes: {
     "./routes/_middleware.tsx": $0,
-    "./routes/api/auth.ts": $1,
+    "./routes/files.ts": $1,
     "./routes/index.tsx": $2,
-    "./routes/login/index.tsx": $3,
+    "./routes/random.ts": $3,
     "./routes/rifff/[rifffId].tsx": $4,
   },
   islands: {
