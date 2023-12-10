77cc94c523f72f9d237e8e6c36e1f56d4e1e6f16
jgusta <jgustafx@gmail.com>
2023-12-09 20:50:44 -0800

    give in and make rifff file a ts file

--- routes/random.ts
+++ routes/random.ts
@@ -1,11 +1,11 @@
 import type { Handlers } from "$fresh/server.ts";
-// import file from "../static/topRifffs.json" with { type: "json" };
 import { env } from 'config';
-import topRifffs from "@/static/topRifffs.json"  with { type: "json" };
+
 export const handler: Handlers = {
   async GET(req, ctx) {
     // const files = JSON.parse(await Deno.readTextFile("static/topRifffs.json"));
-
+    const topRifffs = await import("../util/topRifffs.ts")
+    if (!Array.isArray(topRifffs)) throw new Error("topRifffs is not an array");
     const rifff_id = topRifffs[(topRifffs.length * Math.random()) | 0];
     return new Response(new Blob(), {
       status: 307,
---
+++ util.ts
@@ -0,0 +1,23 @@
+const checkId = function checkId(input:string):Promise<boolean> {
+  // console.log(`checkid run: ${input}`);
+  return new Promise((res) => {
+    const regex = /^[a-f0-9A-F]{32}$/;
+    const check = input.match(regex);
+    if (check == null) {
+      res(false);
+    }
+    res(true);
+  });
+}
+type Headers = {
+  [key: string]: Array<string>
+}
+const headers:Headers = {
+  "html": ["content-type", "text/html"],
+  "css": ["content-type", "text/css"],
+  "ico": ["content-type", "image/x-icon"]
+};
+
+
+
+export { headers, checkId}
